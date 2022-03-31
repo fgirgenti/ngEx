0dad07127281721bda2575adba690f2023290099
Francesco Girgenti <girgenti65@gmail.com>
2022-03-31 17:47:24 +0200

    ngEx3

---
+++ src/app/models/User.ts
@@ -0,0 +1,29 @@
+export enum Role {
+  STAFF = 'staff',
+  STUDENT = 'student',
+  MANAGER = 'manager',
+  ADMIN = 'admin',
+}
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  OTHER = 'other',
+}
+
+export interface User {
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  dateOfBirth: string;
+  address: {
+    city: string;
+    street: string;
+    postalCode: string;
+  };
+  role: Role;
+  username: string;
+  profilePhotoUrl: string;
+  gender: Gender;
+}
