6def793233820b055241d5c130e5c320dbcffdde
Francesco Girgenti <girgenti65@gmail.com>
2022-03-31 14:36:25 +0200

    ngEx1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './components/user/user-container/user-container.component';
+import { OrderContainerComponent } from './components/order/order-container/order-container.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { UserDetailComponent } from './components/user/user-detail/user-detail.component';
+import { UserSingleComponent } from './components/user/user-single/user-single.component';
+import { OrderListComponent } from './components/order/order-list/order-list.component';
+import { OrderSingleComponent } from './components/order/order-single/order-single.component';
+import { OrderDetailComponent } from './components/order/order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    OrderContainerComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserSingleComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule
